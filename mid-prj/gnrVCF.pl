9c28094dab7f1f6d625848414ceecc4d073f13ec
WhiteCrow <11510511@mail.sustc.edu.cn>
2018-04-23 15:17:29 +0800

    how to convert sam to pileup

--- mid-prj/gnrVCF.pl
+++ mid-prj/gnrVCF.pl
@@ -25,25 +25,24 @@ while ( $ref = <genome> ){
 
 # Generate Pileup file from sam file
 while ( $sam_line =  <sam> ){ 
-	if ( (substr($sam_line, 0, 1 ) ne '@') && ($sam_line[3] ne '0')){
-		# unexpend the sam info
 		@sam_line= split("\t",$sam_line);
-		$pileup_line[0] = "Chrom";  # CHROM
-	  $pileup_line[1] = $sam_line[3] ; # 1-based coordinate
-		print $pileup_line[1];
-		$pileup_line[3] = 0;#the number of reads covering the site
-		$pileup_line[4] = ""; # mapping result
-		while( $base_sam = substr($sam_line[9], $pileup_line[1]-$sam_line[3], 1)){
-			$pileup_line[2] = substr($ref_genome[1], $pileup_line[1]-1, 1); # ref base
-			if ( $base_sam eq $pileup_line[2]){
-				$pileup_line[3] = $pileup_line[2] +1;
+		if ( (substr($sam_line, 0, 1 ) ne '@') && $sam_line[3] ne '0'){
+			# unexpend the sam info
+			$pileup_line[0] = "Chrom";       # CHROM
+	  	$pileup_line[1] = $sam_line[3];  # 1-based coordinate
+			$pileup_line[3] = 0;             # number of reads covering the site
+			$pileup_line[4] = "";            # mapping result
+			while( $base_sam = substr($sam_line[9], $pileup_line[1]-$sam_line[3], 1)){
+				$pileup_line[2] = substr($ref_genome[1], $pileup_line[1]-1, 1); # ref base
+				if ( $base_sam eq $pileup_line[2]){
+					$pileup_line[3] = $pileup_line[2] +1;
+				}
+				else{
+					$pileup_line[4] = $pileup_line[4].$base_sam;
+				}	
+				$pileup_line[1] = $pileup_line[1]+1;
 			}
-			else{
-				$pileup_line[4] = $pileup_line[4].$base_sam;
-			}
-			$pileup_line[1] = $pileup_line[1]+1;
+			print join("\t",@pileup_line);
+			print "\n";
 		}
-	}
-	#print join("\t",$pileup_line[1]);
-	print "\n";
 }
